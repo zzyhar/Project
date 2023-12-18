0b84204dbbe6a5dfb5caccf35267c5448af96dcf
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2023-12-17 19:54:41 -0500

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,47 +1,43 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {signInFailure, signInStart, signInSuccess} from '../redux/user/userSlice'
 
-export default function SignIp() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const [error, setError] = useState(null); 
-  const [loading, setLoading] = useState(false); 
-
-
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      setLoading(true);
-    const res = await fetch('/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.success === false){ 
-      setLoading(false);
-      setError(data.message); 
-      return;
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    setLoading(false);
-    setError(null);
-    navigate('/');
-    }catch(error){
-      setLoading(false);
-      setError(error.message);
-    }
-
   };
-  console.log(formData);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -60,4 +56,4 @@ export default function SignIp() {
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
-}
+};
