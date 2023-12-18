8556f4c39852c8f90ab67531bdf5a1ecc316d3af
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2023-12-18 03:19:59 -0500

    Create update user API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -22,6 +23,9 @@ app.listen(3000, () => {
 })
 
 app.use(express.json())
+
+app.use(cookieParser());
+
 // midware
 app.use((e, req, res, next) => { 
     const statusCode = e.statusCode || 500; 
