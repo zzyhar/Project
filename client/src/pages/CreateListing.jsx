db843b577d795f8486dcf91f8bea6ad206e37135
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-01-16 01:19:29 -0500

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -244,7 +244,9 @@ export default function CreateListing(){
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -261,7 +263,9 @@ export default function CreateListing(){
               />
               <div className='flex flex-col items-center'>
                 <p>Discounted price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             ) }
