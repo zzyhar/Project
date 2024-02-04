d2fc740a7f1e094cff67dfb84adec8a7cea05dbb
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-02-04 00:50:01 -0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,7 +14,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
-
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,6 +27,13 @@ app.listen(3000, () => {
 app.use(express.json())
 
 app.use(cookieParser());
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 // midware
 app.use((e, req, res, next) => { 
